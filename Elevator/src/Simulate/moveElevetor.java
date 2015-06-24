bf8275079cd64a15cd67a5be76f980dd443e1e2b
praj25 <sailprajakta@yahoo.in>
2015-06-24 15:57:35 +0530

    First Implementation

---
+++ Elevator/src/Simulate/moveElevetor.java
@@ -0,0 +1,17 @@
+package Simulate;
+
+public class moveElevetor {
+	int curFloor,desFloor;
+	
+	moveElevetor(int curFloor,int desFloor)
+	{
+		this.curFloor=curFloor;
+		this.desFloor=desFloor;
+	}
+	
+	void move()
+	{
+		System.out.println("Moving.........");
+	}
+
+}
