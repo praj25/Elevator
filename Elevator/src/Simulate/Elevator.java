afe423441444e7dc9cf992df9c44ffec5c30ccf9
praj25 <sailprajakta@yahoo.in>
2015-07-01 14:51:53 +0530

    Update Elevator.java

--- Elevator/src/Simulate/Elevator.java
+++ Elevator/src/Simulate/Elevator.java
@@ -5,6 +5,11 @@ public class Elevator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Floor f = new Floor();
+		moveElevetor e = new moveElevetor();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Floor.");
+		int floor = s.nextInt();
+	        e.move(floor,f);
 		
 	}
 
